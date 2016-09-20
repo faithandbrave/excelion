7e982001b0c9f756c8c8bff54a7ac88f56b0362b
Akira Takahashi <faithandbrave@gmail.com>
2016-09-20 22:14:27 +0900

    add meta information for Hex.

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Excelion.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
@@ -27,6 +29,22 @@ defmodule Excelion.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:xlsx_parser, "~> 0.0.7"}]
+    [{:ex_doc, ">= 0.0.0", only: :dev},
+     {:xlsx_parser, "~> 0.0.7"}]
+  end
+
+  defp description do
+    """
+    Excel (xlsx) file reader for Elixir.
+    """
+  end
+
+  defp package do
+    [# These are the default files included in the package
+     name: :excelion,
+     files: ["lib", "priv", "mix.exs", "README*", "LICENSE*"],
+     maintainers: ["Akira Takahashi"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/faithandbrave/excelion"}]
   end
 end
