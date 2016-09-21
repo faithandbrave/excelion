f9685ce9bd80216366643eed676aad96fe55e7c9
Akira Takahashi <faithandbrave@gmail.com>
2016-09-21 19:15:41 +0900

    add docs

--- lib/excelion.ex
+++ lib/excelion.ex
@@ -1,22 +1,50 @@
 defmodule Excelion do
-  # Overview
-  #   The function returns worksheet name list of specifiled .xlsx file.
-  #   If fail get content, return `:error`.
-  # Parameters
-  #   `path` : .xlsx file path
-  #   `zip` : An optional parameter of the zip processing module is allowed (for testing purposes).
+  @moduledoc """
+  Excel (xlsx) file reader for Elixir.
+
+  The library is [excellent](https://hex.pm/packages/excellent) like interface wrapper of [xlsx\_parser](https://hex.pm/packages/xlsx_parser).
+  """
+
+  @doc """
+  The function returns worksheet name list of specifiled .xlsx file. If fail get content, return `:error`.
+
+  ## Parameters
+  - `path` : .xlsx file path
+  - `zip` : An optional parameter of the zip processing module is allowed (for testing purposes).
+
+  ## Example
+  ```elixir
+  > Excelion.get_worksheet_names("test.xlsx")
+  > ["sheet1", "sheet2", "sheet3"]
+  ```
+  """
   @spec get_worksheet_names(String.t, module) :: {:ok, [String.t]} | {:error, String.t}
   def get_worksheet_names(path, zip \\ :zip) do
     XlsxParser.get_worksheet_names(path, zip)
   end
 
-  # Overview
-  #   The function parse .xlsx file. If fail get content, return `:error.`
-  # Parameters
-  #   `path` : .xlsx file path
-  #   `sheet_number` : 0-based sheet number
-  #   `start_row` : first use row number (1-based)
-  #   `zip` : An optional parameter of the zip processing module is allowed (for testing purposes).
+  @doc """
+  The function parse .xlsx file. If fail get content, return `:error.`
+
+  ## Parameters
+  - `path` : .xlsx file path
+  - `sheet_number` : 0-based sheet number
+  - `start_row` : first use row number (1-based)
+  - `zip` : An optional parameter of the zip processing module is allowed (for testing purposes).
+
+  ## Example
+  ```elixir
+  > sheet_number = 0
+  > start_row = 5
+  > Excelion.parse("test.xlsx", sheet_number, start_row) |> elem(1)
+  > [
+  >   ["ID", "name", "description", "value"],
+  >   ["1",  "aaa",  "bbb",         "4"],
+  >   ["2",  "ccc",  "",            "5"], # empty cell to be empty string
+  >   ["3",  "eee",  "fff",         "6"]
+  > ]
+  ```
+  """
   @spec parse(String.t, integer, integer, module) :: {:ok, [[String.t]]} | {:error, String.t}
   def parse(path, sheet_number, start_row, zip \\ :zip) do
     case XlsxParser.get_sheet_content(path, sheet_number + 1, zip) do
@@ -41,13 +69,28 @@ defmodule Excelion do
     end
   end
 
-  # Overview
-  #   The function parse .xlsx file. If fail get content, raise `File.Error` exception.
-  # Parameters
-  #   `path` : .xlsx file path
-  #   `sheet_number` : 0-based sheet number
-  #   `start_row` : first use row number (1-based)
-  #   `zip` : An optional parameter of the zip processing module is allowed (for testing purposes).
+  @doc """
+  The function parse .xlsx file. If fail get content, raise `File.Error` exception.
+
+  ## Parameters
+  - `path` : .xlsx file path
+  - `sheet_number` : 0-based sheet number
+  - `start_row` : first use row number (1-based)
+  - `zip` : An optional parameter of the zip processing module is allowed (for testing purposes).
+
+  ## Example
+  ```elixir
+  > sheet_number = 0
+  > start_row = 5
+  > Excelion.parse!("test.xlsx", sheet_number, start_row)
+  > [
+  >   ["ID", "name", "description", "value"],
+  >   ["1",  "aaa",  "bbb",         "4"],
+  >   ["2",  "ccc",  "",            "5"], # empty cell to be empty string
+  >   ["3",  "eee",  "fff",         "6"]
+  > ]
+  ```
+  """
   @spec parse!(String.t, integer, integer, module) :: [[String.t]] | no_return
   def parse!(path, sheet_number, start_row, zip \\ :zip) do
     case parse(path, sheet_number, start_row, zip) do
