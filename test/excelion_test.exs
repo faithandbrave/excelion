76cb86c5b4156f65da0e87c9429a1305190e211f
Akira Takahashi <faithandbrave@gmail.com>
2016-09-23 15:21:19 +0900

    add all case test

--- test/excelion_test.exs
+++ test/excelion_test.exs
@@ -2,15 +2,23 @@ defmodule ExcelionTest do
   use ExUnit.Case
   doctest Excelion
 
+  def collect_filename, do: "priv/test.xlsx"
+  def bad_filename, do: "priv/__not_found_file.xlsx"
+
   test "get sheet names" do
-    ret = Excelion.get_worksheet_names("priv/test.xlsx")
+    ret = Excelion.get_worksheet_names(collect_filename)
     assert ret == {:ok, ["test_sheet"]}
   end
 
-  test "parse xlsx file" do
+  test "get sheet names error" do
+    ret = Excelion.get_worksheet_names(bad_filename)
+    assert elem(ret, 0) == :error
+  end
+
+  test "parse! xlsx file" do
     sheet_number = 0
     start_row = 5
-    ret = Excelion.parse!("priv/test.xlsx", sheet_number, start_row)
+    ret = Excelion.parse!(collect_filename, sheet_number, start_row)
     expected = [
       ["ID", "name", "description", "value"],
       ["1",  "aaa",  "bbb",         "4"],
@@ -18,5 +26,27 @@ defmodule ExcelionTest do
       ["3",  "eee",  "fff",         "6"]
     ]
     assert ret == expected
+  end
+
+  test "parse! xlsx file error" do
+    assert_raise File.Error, fn ->
+      Excelion.parse!(bad_filename, 0, 5)
+    end
+  end
+
+  test "parse xlsx file" do
+    ret = Excelion.parse(collect_filename, 0, 5)
+    expected = [
+      ["ID", "name", "description", "value"],
+      ["1",  "aaa",  "bbb",         "4"],
+      ["2",  "ccc",  "",            "5"], # empty cell to be empty string
+      ["3",  "eee",  "fff",         "6"]
+    ]
+    assert ret == {:ok, expected}
+  end
+
+  test "parse xlsx file error" do
+    ret = Excelion.parse(bad_filename, 0, 5)
+    assert elem(ret, 0) == :error
   end
 end
