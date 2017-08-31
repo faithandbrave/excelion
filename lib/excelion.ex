ed3201c28e011d2ec9fe4876af18ba8c4dbd37ed
Akira Takahashi <faithandbrave@gmail.com>
2017-08-31 15:32:28 +0900

    support over 26 columns. now available 52 columns

--- lib/excelion.ex
+++ lib/excelion.ex
@@ -101,7 +101,9 @@ defmodule Excelion do
 
   @spec alphas() :: [String.t]
   defp alphas do
-    ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+     "AA","AB","AC","AD","AE","AF","AG","AH","AI","AJ","AK","AL","AM","AN","AO","AP","AQ","AR","AS",
+     "AT","AU","AV","AW","AX","AY","AZ"]
   end
 
   @spec alpha_to_index(String.t) :: integer
